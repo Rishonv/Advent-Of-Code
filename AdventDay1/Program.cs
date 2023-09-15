8bf3042637880adde0c94c68b06b0937030a6b40
Rishon Dsouza <rishonv.dsouza@gmail.com>
2023-09-15 18:10:00 +0400

    Initial

---
+++ AdventDay1/Program.cs
@@ -0,0 +1,24 @@
+﻿string line = "";
+StreamReader sr = new StreamReader("./Input.txt");
+List<List<int>> ElfCalories = new List<List<int>>();
+ElfCalories.Add(new List<int>());
+
+while (line != null)
+{
+    if (line.Length == 0)
+    {
+        ElfCalories.Add(new List<int>());
+    }
+    else
+    {
+        ElfCalories.Last().Add(Int32.Parse(line));
+    }
+    line = sr.ReadLine();
+    Console.WriteLine(line);
+}
+sr.Close();
+
+// List<int> sumElves= ElfCalories.ConvertAll(list => list.Sum());
+// sumElves.OrderByDescending(l => l);
+// Console.WriteLine("string %d %d %d", sumElves[0], sumElves[1],                                                     sumElves[2]);
+Console.WriteLine(ElfCalories.ConvertAll(list => list.Sum()).OrderByDescending(l => l).Take(3).Sum());
---
+++ AdventDay2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography;
+
+string? line = "";
+StreamReader sr = new StreamReader("./Input.txt");
+int totalScore = 0;
+
+line = sr.ReadLine();
+while (line != null)
+{
+    Console.WriteLine(line);
+    (char, char) plays = (line[0], line[2]);
+
+    int score = plays switch
+    {
+        ('A', 'X') => 3,
+        ('A', 'Y') => 1,
+        ('A', 'Z') => 2,
+        ('B', 'X') => 1,
+        ('B', 'Y') => 2,
+        ('B', 'Z') => 3,
+        ('C', 'X') => 2,
+        ('C', 'Y') => 3,
+        ('C', 'Z') => 1,
+        (_, _) => 0
+    };
+
+    int outcome = line[2] switch
+    {
+        'X' => 0,
+        'Y' => 3,
+        'Z' => 6,
+        _ => 0
+    };
+
+    line = sr.ReadLine();
+    totalScore += score + outcome;
+}
+Console.WriteLine(totalScore);
